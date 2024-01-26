c205df689dc4c6e5e01a1e52be5d03614ef192f7
RinaShabani2 <150968383+RinaShabani2@users.noreply.github.com>
2024-01-26 15:14:23 +0100

    Update Phase1.py

--- Phase1.py
+++ Phase1.py
@@ -3,7 +3,6 @@ import csv
 import pandas as pd
 from concurrent.futures import ThreadPoolExecutor
 
-# Function to process individual rows
 def process_row(row):
     try:
         longitude = float(row['Longitute'])
@@ -16,7 +15,7 @@ def process_row(row):
             road = address.get('road', 'N/A')
             street = address.get('street', 'N/A')
             if road == 'N/A' and street == 'N/A':
-                return None  # Don't send back if both road and street are 'N/A'
+                return None  
             else:
                 return (latitude, longitude, road, street)
         else:
@@ -26,7 +25,6 @@ def process_row(row):
         return None
 
 def process_chunk(chunk):
-    # Apply filters as per your requirements
     scenario_1_filtered = chunk[~((chunk['Di1'] == 0) & (chunk['Di3'] == 1))]
     scenario_2_filtered = chunk[~((chunk['Di1'] == 0) & (chunk['Di3'] == 0))]
     chunk = pd.concat([scenario_1_filtered, scenario_2_filtered])
@@ -36,32 +34,21 @@ def process_chunk(chunk):
         results = list(executor.map(process_row, [row for _, row in chunk.iterrows() if not pd.isnull(row['Latitude']) and not pd.isnull(row['Longitute'])]))
     return [result for result in results if result is not None]
 
-# Input path for CSV file and output filename
 input_path = input("Enter the path to your CSV file: ")
 output_filename = input("Enter the path to your output file: ")
 
-# Read the CSV file into a DataFrame
 df = pd.read_csv(input_path, parse_dates=['DeviceDateTime'])
 
 unique_dates = df['DeviceDateTime'].dt.date.unique()
 
 for date in unique_dates:
-    # Filter the DataFrame for the current date
     filtered_df = df[df['DeviceDateTime'].dt.date == date]
-
-    # Apply the process_chunk function to the filtered DataFrame
     filtered_results = process_chunk(filtered_df)
-
-    # Extract unique road names from filtered results
     unique_roads = list(set(result[2] for result in filtered_results))
 
-    # Sanitize road names by replacing spaces with underscores
     sanitized_unique_roads = [road.replace(' ', '_') for road in unique_roads]
 
-    # Concatenate unique road names into a single string with spaces
     unique_roads_str = '\n'.join(sanitized_unique_roads)
-
-    # Write unique road names to a new CSV file for the current date
     with open(output_filename, 'a', newline='', encoding='utf-8') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow([f'Date: {date}'])
